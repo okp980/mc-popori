97824a0f037b27834a1e42b2ec10efe80d1995e7
Emmanuel Okp <okpunorrex@gmail.com>
2022-05-30 16:21:24 +0100

    first commit

---
+++ pages/_document.tsx
@@ -0,0 +1,80 @@
+import Document, {
+	DocumentContext,
+	DocumentInitialProps,
+	Html,
+	Main,
+	NextScript,
+	Head,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+	static async getInitialProps(
+		ctx: DocumentContext
+	): Promise<DocumentInitialProps> {
+		const sheet = new ServerStyleSheet();
+		const originalRenderPage = ctx.renderPage;
+
+		try {
+			ctx.renderPage = () =>
+				originalRenderPage({
+					enhanceApp: (App) => (props) =>
+						sheet.collectStyles(<App {...props} />),
+				});
+
+			const initialProps = await Document.getInitialProps(ctx);
+			return {
+				...initialProps,
+				styles: [
+					<>
+						{initialProps.styles}
+						{sheet.getStyleElement()}
+					</>,
+				],
+			};
+		} finally {
+			sheet.seal();
+		}
+	}
+
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					<link
+						rel="preload"
+						href="/fonts/Spartan-ExtraLight.ttf"
+						as="font"
+						type="font/woff"
+						crossOrigin=""
+					/>
+					<link
+						rel="preload"
+						href="/fonts/Spartan-Regular.ttf"
+						as="font"
+						type="font/woff"
+						crossOrigin=""
+					/>
+					<link
+						rel="preload"
+						href="/fonts/Spartan-SemiBold.ttf"
+						as="font"
+						type="font/woff"
+						crossOrigin=""
+					/>
+					<link
+						rel="preload"
+						href="/fonts/Spartan-ExtraBold.ttf"
+						as="font"
+						type="font/woff"
+						crossOrigin=""
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
